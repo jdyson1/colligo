a485392dac82bbdc01020e487987f267d2ce5143
Jesse Dyson <Jesse.Dyson@advhomecare.org>
2018-08-10 13:42:16 -0400

    First commit

---
+++ Colligo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Colligo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
